bd649b8b2ae7e8ebf1c8446503e2fff0079b407e
Krzy221 <79155912+Krzy221@users.noreply.github.com>
2021-02-19 18:29:17 +0100

    first commit

---
+++ src/Frame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+    private static final long serialVersionUID = 1L;
+    public Frame(){
+        super("Kółko i krzyżyk");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(640,400);
+        setLocation(2000,51);
+        setVisible(true);
+
+    }
+}
